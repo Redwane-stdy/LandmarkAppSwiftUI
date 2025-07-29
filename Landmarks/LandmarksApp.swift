3423f3e6f439eeaec80a6bd5a1073bfcf27e2e63
Redwane-stdy <redwane.aitbrahim67@gmail.com>
2025-07-29 10:09:50 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Redwane Ait Brahim on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
